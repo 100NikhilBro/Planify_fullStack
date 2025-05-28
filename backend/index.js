04c0c337d5be4bb45603e23338811029e02bb3ab
100NikhilBro <100pranjalgupta@gmail.com>
2025-05-29 03:39:44 +0530

    Update CORS allowed origins for production frontend URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4567;
 
 const cors = require('cors');
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://fanciful-basbousa-7c8cca.netlify.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
 }))
