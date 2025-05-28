40623e9af82fad6bed915191c11feef1b2ab857f
100NikhilBro <100pranjalgupta@gmail.com>
2025-05-29 04:58:44 +0530

    some more changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,59 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd()); // load .env variables
-
-    return {
-        server: {
-            proxy: {
-                '/task': env.VITE_API_URL,
-                '/user': env.VITE_API_URL,
-            }
-        },
-        plugins: [react(), tailwindcss()],
-    };
-});
-
-
-
-
-
-
-
-// import { defineConfig } from 'vite';
+// import { defineConfig, loadEnv } from 'vite';
 // import react from '@vitejs/plugin-react';
 // import tailwindcss from '@tailwindcss/vite';
 
-// export default defineConfig(() => {
-//     // const env = loadEnv(mode, process.cwd()); // load .env variables
-
-//     console.log()
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, process.cwd()); // load .env variables
 
 //     return {
 //         server: {
 //             proxy: {
-//                 '/task': "http://localhost:7892",
-//                 '/user': "http://localhost:7892",
+//                 '/task': env.VITE_API_URL,
+//                 '/user': env.VITE_API_URL,
 //             }
 //         },
 //         plugins: [react(), tailwindcss()],
 //     };
 // });
+
+
+
+
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig(() => {
+    // const env = loadEnv(mode, process.cwd()); // load .env variables
+
+    console.log()
+
+    return {
+        // server: {
+        //     proxy: {
+        //         '/task': "http://localhost:7892",
+        //         '/user': "http://localhost:7892",
+        //     }
+        // },
+        proxy: {
+            '/task': {
+                target: 'https://planify-full-stack.vercel.app',
+                changeOrigin: true,
+                secure: true,
+            },
+            '/user': {
+                target: 'https://planify-full-stack.vercel.app',
+                changeOrigin: true,
+                secure: true,
+            },
+        },
+
+        plugins: [react(), tailwindcss()],
+    };
+});
 
 
 
